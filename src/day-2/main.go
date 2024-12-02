2b46788dd095c7cf36f7d20df3261823cc18a8e7
Harry Ross <hazross@hotmail.com>
2024-12-02 18:46:20 +1100

    faulty part 2

--- src/day-2/main.go
+++ src/day-2/main.go
@@ -11,6 +11,9 @@ func main() {
 	lines := filehelpers.GetLines("input.txt")
 	res := part1(lines)
 	fmt.Println(res)
+
+	res = part2(lines)
+	fmt.Println(res)
 }
 
 func part1(lines []string) int {
@@ -34,12 +37,7 @@ func part1(lines []string) int {
 
 			diff := currNum - lastNum
 
-			if diff > 3 || diff == 0 || diff < -3 {
-				unsafe = true
-				break
-			}
-
-			if (diff > 0 && lastDiff < 0) || (diff < 0 && lastDiff > 0) {
+			if isUnsafe(lastDiff, diff) {
 				unsafe = true
 				break
 			}
@@ -57,5 +55,58 @@ func part1(lines []string) int {
 }
 
 func part2(lines []string) int {
-	return 0
+	unsafeCount := 0
+
+	for _, line := range lines {
+		splitNums := strings.Split(line, " ")
+
+		firstNum, err := strconv.Atoi(splitNums[0])
+		if err != nil {
+			panic(err)
+		}
+		lastNum := firstNum
+		lastDiff := 0
+		unsafe := false
+		for i, num := range splitNums[1:] {
+
+			currNum, err := strconv.Atoi(num)
+			if err != nil {
+				panic(err)
+			}
+
+			diff := currNum - lastNum
+
+			if isUnsafe(lastDiff, diff) {
+				if i != len(splitNums[1:])-1 {
+
+					nextNum, err := strconv.Atoi(splitNums[i+2])
+					if err != nil {
+						panic(err)
+					}
+
+					diff = nextNum - lastNum
+					if isUnsafe(lastDiff, diff) {
+						unsafe = true
+						break
+					}
+				} else {
+					unsafe = true
+					break
+				}
+			}
+
+			lastNum = currNum
+			lastDiff = diff
+		}
+
+		if unsafe {
+			unsafeCount++
+		}
+	}
+
+	return len(lines) - unsafeCount
+}
+
+func isUnsafe(lastDiff int, diff int) bool {
+	return (diff > 3 || diff == 0 || diff < -3) || (diff > 0 && lastDiff < 0) || (diff < 0 && lastDiff > 0)
 }
